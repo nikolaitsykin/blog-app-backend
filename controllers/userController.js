8acfb4692b020ef639bfada29a706fe3794b212b
flipexe <104075088+flipexe@users.noreply.github.com>
2024-06-06 13:35:10 -0400

    User schema and user actions have been added

---
+++ controllers/userController.js
@@ -0,0 +1,98 @@
+import { validationResult } from "express-validator";
+import userModel from "../models/User.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new userModel({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: hash,
+      avatarUrl: req.body.avatarUrl,
+    });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to register",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Invalid login or password",
+      });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Invalid login or password",
+      });
+    }
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+    const { passwordHash, ...userData } = user._doc;
+    res.json({ ...userData, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to login",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const { passwordHash, ...userData } = user._doc;
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to get user",
+    });
+  }
+};
