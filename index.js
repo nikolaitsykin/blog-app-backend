d8eb23b4742fd894de7c7ac5d621921a588fb517
flipexe <104075088+flipexe@users.noreply.github.com>
2024-06-06 13:38:20 -0400

    User and post requests have been added to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import {
+  validateRegister,
+  validateLogin,
+  validatePost,
+} from "./validations/validations.js";
+import { checkAuth } from "./utils/checkAuth.js";
+import * as userController from "./controllers/userController.js";
+import * as postController from "./controllers/postController.js";
 
 mongoose
   .connect(
@@ -17,7 +24,15 @@ const app = express();
 const port = 4000;
 app.use(express.json());
 
+app.post("/auth/login", validateLogin, userController.login);
+app.post("/auth/register", validateRegister, userController.register);
+app.get("/auth/me", checkAuth, userController.getMe);
 
+app.get("/posts", checkAuth, validatePost, postController.getAll);
+app.get("/posts/:id", postController.getOne);
+app.post("/posts", checkAuth, validatePost, postController.create);
+app.delete("/posts/:id", checkAuth, postController.remove);
+app.patch("/posts/:id", checkAuth, postController.update);
 
 app.listen(port, (err) => {
   if (err) throw err;
