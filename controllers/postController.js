4056397f4b18aaa07e25e693a0853872ca0caa6f
flipexe <104075088+flipexe@users.noreply.github.com>
2024-06-06 13:36:20 -0400

    Post schema, post actions and check auth have been implemented

---
+++ controllers/postController.js
@@ -0,0 +1,108 @@
+import postModel from "../models/Post.js";
+
+export const create = async (req, res) => {
+  try {
+    const doc = new postModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+    const post = await doc.save();
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to create post",
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await postModel.find().populate("user").exec();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to get posts",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await postModel
+      .findOneAndUpdate(
+        { _id: postId },
+        { $inc: { viewsCount: 1 } },
+        { returnDocument: "after" }
+      )
+      .populate("user")
+      .exec();
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to get post",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await postModel.findByIdAndDelete({
+      _id: postId,
+    });
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to delete post",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await postModel.findByIdAndUpdate(
+      { _id: postId },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      }
+    );
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Failed to update post",
+    });
+  }
+};
---
+++ validations/validations.js
@@ -0,0 +1,52 @@
+import { body } from "express-validator";
+
+export const validateRegister = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 3 }, { max: 32 })
+    .withMessage("Name length must be between 3 and 32."),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password length must be at least 6"),
+  body("avatarUrl").optional().isURL().withMessage("Invalid URL"),
+];
+
+export const validateLogin = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password length must be at least 6"),
+];
+
+export const validatePost = [
+  body("title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .isLength({ min: 3 })
+    .withMessage("Title length must be at least 3"),
+  body("text")
+    .notEmpty()
+    .withMessage("Text is required")
+    .isLength({ min: 3 })
+    .withMessage("Text length must be at least 3"),
+  body("tags")
+    .optional()
+    .isLength({ min: 1 })
+    .withMessage("Tags are required")
+    .isString(),
+  body("imageUrl").optional().isURL().withMessage("Invalid URL").isString(),
+];
